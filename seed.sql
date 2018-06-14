956e89c40f2126ed3db4d7e4b1c19c2d20d1d18b
TECH-A58 <tech-a58@TECH-A58s-MacBook-Air.local>
2018-06-14 18:42:27 +0100

    connected seed file with psql

--- seed.sql
+++ seed.sql
@@ -8,7 +8,7 @@ CREATE TABLE club (
 
 );
 
-INSERT INTO post (title , body) VALUES ('club 1' , 'Body text 1');
-INSERT INTO post (title , body) VALUES ('club 2' , 'Body text 2');
-INSERT INTO post (title , body) VALUES ('club 3' , 'Body text 3');
-INSERT INTO post (title , body) VALUES ('club 4' , 'Body text 4');
+INSERT INTO club (title , body) VALUES ('club 1' , 'Body text 1');
+INSERT INTO club (title , body) VALUES ('club 2' , 'Body text 2');
+INSERT INTO club (title , body) VALUES ('club 3' , 'Body text 3');
+INSERT INTO club (title , body) VALUES ('club 4' , 'Body text 4');
