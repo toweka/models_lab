fe1165a70b411f778c0c9a3ebd1ff8e38adeaf8a
TECH-A58 <tech-a58@TECH-A58s-MacBook-Air.local>
2018-06-14 18:33:55 +0100

    created seed file

---
+++ seed.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS club;
+
+CREATE TABLE club (
+
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  body TEXT
+
+);
+
+INSERT INTO post (title , body) VALUES ('club 1' , 'Body text 1');
+INSERT INTO post (title , body) VALUES ('club 2' , 'Body text 2');
+INSERT INTO post (title , body) VALUES ('club 3' , 'Body text 3');
+INSERT INTO post (title , body) VALUES ('club 4' , 'Body text 4');
