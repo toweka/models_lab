83d9e1fd38023b16409a2c800795164db7f10bfb
TECH-A58 <tech-a58@TECH-A58s-MacBook-Air.local>
2018-06-14 18:56:34 +0100

    Created the clubs controller

--- config.ru
+++ config.ru
@@ -2,7 +2,7 @@ require "sinatra"
 require "sinatra/contrib"
 require "sinatra/reloader" if development?
 require "pg"
-require_relative "../sinatra_intro/controllers/club_controller.rb"
+require_relative "../sinatra_intro/controllers/clubs_controller.rb"
 require_relative "../sinatra_intro/models/club.rb"
 
 use Rack::MethodOverride
---
+++ controllers/clubs_controller.rb
@@ -0,0 +1,92 @@
+class ClubsController < Sinatra::Base
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  # sets root as the parent-directory of the current file
+  set :root, File.join(File.dirname(__FILE__), "..")
+
+  # sets the view directory correctly
+  set :views, Proc.new { File.join(root, "views") }
+
+  get '/' do
+    "<a href='/clubs'>Clubs Index</a>
+    <br>
+    <h1>Main Index Page</h1>"
+
+    @clubs = Club.all
+
+  end
+
+  # Index page
+  get '/clubs' do
+    @title = "Blog Clubs"
+    @clubs = Club.all
+    erb :'clubs/index'
+  end
+
+  # New page
+  get '/clubs/new' do
+    @club = Club.new
+
+    erb :"clubs/new"
+  end
+
+  # Create page
+  club "/clubs/" do
+    club = Club.new
+
+    club.title = params[:title]
+    club.body = params[:body]
+
+    club.save
+
+    redirect '/clubs'
+
+  end
+
+  # Show page
+  get '/clubs/:id' do
+
+    id = params[:id].to_i
+
+    @club = Club.find(id)
+
+    erb :"clubs/show"
+
+  end
+
+  # Edit page
+  get "/clubs/:id/edit" do
+    id = params[:id].to_i
+
+    @club = Club.find(id)
+
+    erb :"clubs/edit"
+  end
+
+  # Update page
+  put "/clubs/:id" do
+    id = params[:id].to_i
+
+    club = Club.find(id)
+
+    club.title = params[:title]
+    club.body = params[:body]
+
+    club.save
+
+    redirect "/clubs/#{id}"
+  end
+
+  # Destroy page
+  delete "/clubs/:id" do
+    id = params[:id].to_i
+
+    Club.destroy(id)
+
+    redirect "/clubs"
+  end
+
+end
